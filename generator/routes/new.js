987ec38e75636c9bdfb16cda00152f1e0a1be65e
j0ojo <71373291+j0ojo@users.noreply.github.com>
2020-11-15 11:35:14 +0100

    tp5

--- generator/routes/new.js
+++ generator/routes/new.js
@@ -1,3 +1,35 @@
+import express from 'express';
+
+var express = require('express');
+var router = express.Router();
+
+const db = [{
+    Title: "Guardians of the Galaxy Vol. 2",
+    id : "0"
+    }];
+
+
+    //NEW ROUTES
+
+router.get('/', function(req, res, next) {
+    const {id} = req.params;
+    const db=_.find(db,["id",id]);
+     
+    res.status(200).json({
+      message:'user found',
+      Title
+    });
+  });
+  
+  router.post('/',function(req,res,next){
+    
+    const temp=req.body;
+    db.push(temp);
+    res.send( 'post route ${temp.Title} added to the database'); //POSTMAN
+    
+  });
+
+
 app.get((req, res) => { 
 
     res.json({
